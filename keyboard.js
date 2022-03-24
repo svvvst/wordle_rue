a9c27c82e9510131ee1406145a1660f5f87f6988
Tamas Kalinics <tkalyn3@gmail.com>
2022-03-24 21:46:33 +0000

    added keyboard; moved css

---
+++ keyboard.js
@@ -0,0 +1,45 @@
+function buildKeyboard(layout) {
+  const keyboardElement = document.querySelector("#keyboard");
+  let keyboard = "";
+
+  layout.forEach((row) => {
+    keyboard += `<div>`;
+    row.forEach((letter) => {
+      if (letter) {
+        if (letter === "backspace") {
+          keyboard += `<button class="key" id="backspace">⇦</button>`;
+        } else {
+          keyboard += `<button class="key" data-letter=${letter}>${letter}</button>`;
+        }
+      }
+    });
+    keyboard += `</div>`;
+  });
+
+  keyboardElement.innerHTML = keyboard;
+  keyboardElement.addEventListener("click", keyPress);
+}
+
+function keyPress(event) {
+  const guessInput = document.querySelector("#inputStr");
+
+  // if backspace, remove last character from the input field
+  // if any other key, add the character (no more than 5)
+  if (event.target.id === "backspace") {
+    guessInput.value = guessInput.value.slice(0, -1);
+  } else if (
+    event.target.classList.contains("key") &&
+    guessInput.value.length < 5
+  ) {
+    guessInput.value += event.target.innerHTML;
+  }
+}
+
+// layout with "false" for unused keyboard positions
+const layout = [
+  ["Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ы"],
+  ["Ф", "І", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Є", false],
+  ["Ґ", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", false, "backspace"],
+];
+
+buildKeyboard(layout);
